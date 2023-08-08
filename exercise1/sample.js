761b6344af14dc62936afc500acaa4886ef9449c
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2023-08-08 18:27:30 -0500

    adding notes

--- exercise1/sample.js
+++ exercise1/sample.js
@@ -7,6 +7,8 @@ import ReactDOM from "react-dom";
  *
  *
  * 1. Create sample page in JSX (> 4 elements and save them in a variable
- * 2. Select the div with the id of 'root' and use .append() to append
+ * 2. Select the div with the id of 'root' and use .append() to append your JSX
+ * 3. See if you can guess what will show up in the browser before running the code
+ * 4. See if you can explain what actually shows up in the broswer
  *
  */
